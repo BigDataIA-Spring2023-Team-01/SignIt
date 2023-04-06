3f1f3fa971982d39e585f784efa4c7682abdaceb
thejas98 <thejasbharadwaj98@gmail.com>
2023-04-06 16:49:40 -0400

    Streamlit

--- api/main.py
+++ api/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 import pandas as pd
+from pydantic import ValidationError, BaseModel
+
 app = FastAPI()
+
+
+class Transcript(BaseModel):
+    transcript: str
+
 
 videos = [
     {"name": "video1.mp4", "duration": 120},
@@ -12,4 +19,29 @@ videos = [
 
 async def get_videos():
     df = pd.read_csv('data/features_df.csv')
-    return df
+    return df.to_dict()
+
+
+
+
+@app.post("/search_video_ids")
+async def search_video_ids(transcript: Transcript):
+    df = pd.read_csv("data/features_df.csv")
+
+    try:
+        words = transcript.transcript.split()
+        video_ids = []
+        for word in words:
+            matching_row = df.loc[df['word'] == word.lower()]
+            if not matching_row.empty:
+                video_ids.append(matching_row.iloc[0]['video_id'])
+            else:
+                letters = list(word.lower())
+                for letter in letters:
+                    matching_row = df.loc[df['word'] == letter]
+                    if not matching_row.empty:
+                        video_ids.append(int(matching_row.iloc[0]['video_id']))
+        print(video_ids)
+        return {'video': [int(id) for id in video_ids]}
+    except ValueError as e:
+        return {"error": str(e)}
--- streamlit/main.py
+++ streamlit/main.py
@@ -5,11 +5,11 @@ import streamlit as st
 API_URL = "http://fastapi.latest:8000"
 
 # Define a function to get the video data from the API
-def get_videos():
-    url = f"{API_URL}/videos"
-    response = requests.get(url)
+def get_video_id(transcript):
+    url = f"{API_URL}/search_video_ids"
+    response = requests.post(url,json={"transcript": transcript})
     if response.status_code == 200:
-        return response.json()
+        return response.text
     else:
         return []
 
@@ -19,11 +19,11 @@ def main():
     st.set_page_config(page_title="My Video App")
 
     # Get the video data from the API
-    videos = get_videos()
+    user_input = st.text_input("Enter text")
 
     # Display the video data in a table
-    st.write(videos)
-
+    res = get_video_id(user_input)
+    st.write(res)
 # Run the Streamlit app
 if __name__ == "__main__":
     main()
