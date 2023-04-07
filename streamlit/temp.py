5a125d1c39a0f8eebf0b560433e0fc59c1a5e3f0
thejas98 <thejasbharadwaj98@gmail.com>
2023-04-07 00:32:44 -0400

    tesst

--- streamlit/temp.py
+++ streamlit/temp.py
@@ -1,17 +1,6 @@
 import requests
 import streamlit as st
 
-# Set the URL for the API endpoint
-API_URL = "http://fastapi.latest:8000"
-
-# Define a function to get the video data from the API
-def get_video_id(transcript):
-    url = f"{API_URL}/search_video_ids"
-    response = requests.post(url,json={"transcript": transcript})
-    if response.status_code == 200:
-        return response.text
-    else:
-        return []
 
 # Define the Streamlit app
 def main():
@@ -21,9 +10,8 @@ def main():
     # Get the video data from the API
     user_input = st.text_input("Enter text")
 
-    # Display the video data in a table
-    res = get_video_id(user_input)
-    st.write(res)
+    video_url = "data/archive/signlanguagevideos/temp.mp4"
+    st.video(video_url)
 # Run the Streamlit app
 if __name__ == "__main__":
     main()
