d9d455a145804aef126ddc1d2e24bcad8dad8623
CY2404 <114796225+CY2404@users.noreply.github.com>
2023-04-06 23:42:14 -0400

    updated arch_diag

--- Arch_Diagram.py
+++ Arch_Diagram.py
@@ -11,3 +11,16 @@ from diagrams.onprem.container import Docker # to denote docker
 from diagrams.custom import Custom # for custom logos : streamlit, whiper api, kaggle
 from diagrams.aws.storage import S3 # to denote aws bucket
 
+#-----------------------------------------------------------------------------------------------
+#Creating the cloud cluster
+with Diagram("Workflow", show=False, direction="LR"):
+  user = Userresource("Users")
+  with Cluster("Cloud"):
+    kaggle = Custom("kaggle","./data/images/kaggle.png")
+    # creating streamlit cloud
+    with Cluster("Streamlit"):
+        with Cluster("Streamlit Cloud"):
+          streamlit_cloud = DirectMail("Streamlit Cloud")
+          streamlit_app = Custom("Streamlit","./data/images/streamlit-logo.png") 
+
+
